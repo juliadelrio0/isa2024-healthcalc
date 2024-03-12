9a5142b9843f1427e4227d635775b35a9799dd72
Julia del Río <juliadelrio2003@uma.es>
2024-03-12 21:38:28 +0100

    Segunda corrección fallos clase HealthCalcTest

--- src/test/java/healthcalc/HealthCalcTest.java
+++ src/test/java/healthcalc/HealthCalcTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestTemplate;
 
 @DisplayName("Tests para la calculadora de salud.")
 public class HealthCalcTest {
