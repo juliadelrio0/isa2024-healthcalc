5f89d38b1da32b614612705c907b209fe62babbf
juliadelrio0 <160472229+juliadelrio0@users.noreply.github.com>
2024-06-04 03:13:14 +0200

    Restauración de la clase HealthCalc debido a equivocación

--- src/main/java/healthcalc/HealthCalc.java
+++ src/main/java/healthcalc/HealthCalc.java
@@ -2,7 +2,7 @@ package healthcalc;
 
 public interface HealthCalc {
 	
-	public float idealWeight(int height, Gender gender) throws Exception;
+	public float idealWeight(int height, char gender) throws Exception;
 
-	public float basalMetabolicRate(float weight, int height, Gender gender, int age) throws Exception;
+	public float basalMetabolicRate(float weight, int height, char gender, int age) throws Exception;
 }
